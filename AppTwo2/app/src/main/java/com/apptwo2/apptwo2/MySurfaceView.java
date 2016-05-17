f576dbf022d54f9376b2a0f930585a83fd4d2c1e
bato6582 <keil.magda@googlemail.com>
2016-05-17 17:00:05 +0200

    clean code

--- AppTwo2/app/src/main/java/com/apptwo2/apptwo2/MySurfaceView.java
+++ AppTwo2/app/src/main/java/com/apptwo2/apptwo2/MySurfaceView.java
@@ -16,21 +16,15 @@ import android.view.View;
 // Creating a SurfaceView class
 // http://android-er.blogspot.de/2014/03/simple-surfaceview-example.html
 
-
 public class MySurfaceView extends View {
 
-    //private SurfaceHolder surfaceHolder;
     private int time = 0;
-    public float[] data_ = new float[3];
+    private float[] data_ = new float[3];
+    private Paint mPaint;
 
-
-    Paint mPaint;
-
-
-    public MySurfaceView(Context context) {
-        super(context);
+    public MySurfaceView(Context context, AttributeSet attrs) {
+        super(context, attrs);
         init();
-
     }
 
     private void init() {
@@ -39,23 +33,8 @@ public class MySurfaceView extends View {
         mPaint.setStrokeWidth(3);
     }
 
-    public MySurfaceView(Context context, AttributeSet attrs) {
-        super(context, attrs);
-        init();
-
-    }
-
-    public MySurfaceView(Context context, AttributeSet attrs, int defStyle) {
-        super(context, attrs, defStyle);
-        init();
-
-    }
-
-
     @Override
     public void onDraw(Canvas canvas){
-
-
         float ratio = getHeight()/20;
 
         System.out.print("onDraw is called!!!!!! \n");
@@ -63,36 +42,29 @@ public class MySurfaceView extends View {
         System.out.print(data_[1]+"\n");
         System.out.print(data_[2]+"\n");
 
-        //if (time == getWidth()) {
-        //    time = 0;
-        //}
-
+        if (time == getWidth()) {
+            time = 0;
+        }
 
         mPaint.setColor(Color.GREEN);
-        canvas.drawPoint(getWidth()/2, (float) ((getHeight() / 2) - ratio * data_[0]), mPaint);
+        canvas.drawPoint((float) time, (float) ((getHeight() / 2) - ratio * data_[0]), mPaint);
         mPaint.setColor(Color.RED);
-        canvas.drawPoint(getWidth()/2, (float) ((getHeight() / 2) - ratio * data_[1]), mPaint);
+        canvas.drawPoint((float) time, (float) ((getHeight() / 2) - ratio * data_[1]), mPaint);
         mPaint.setColor(Color.BLUE);
-        canvas.drawPoint(getWidth()/2, (float) ((getHeight() / 2) - ratio * data_[2]), mPaint);
-        //time++;
-
-
-
+        canvas.drawPoint((float) time, (float) ((getHeight() / 2) - ratio * data_[2]), mPaint);
+        time++;
 
         canvas.drawPoint(getWidth()/2, getHeight()/2, mPaint);
 
-        super.onDraw(canvas);
         // paint.setTextSize(18f);
         // canvas.drawText("0", getWidth()-20f, getHeight()/2, mPaint);
     }
 
-    public float[] saveData(float[] data){
-
+    public void saveData(float[] data){
         this.data_[0] = data[0];
         this.data_[1] = data[1];
         this.data_[2] = data[2];
-        invalidate();
         System.out.println(data_[0]);
-        return data;
+        invalidate();
     }
 }
